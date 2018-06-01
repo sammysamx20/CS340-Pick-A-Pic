1ec830d8284eebbe0048a323b039663ffc2492d1
Sam Young <sammysamx20@gmail.com>
2018-05-31 17:21:55 -0700

    re commint for connect vars and pages

---
+++ connectvars.php
@@ -0,0 +1,8 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_youngsam');
+  define('DB_PASSWORD', '1230984567Asd@');
+  define('DB_NAME', 'cs340_youngsam');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_youngsam');
+?>
---
+++ pages.php
@@ -0,0 +1,9 @@
+<?php
+//$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
+	$content = array(
+		"Add Supplier" => "addSupplier.php",
+		"Add Part" => "addPart.php",
+		"List Parts" => "listParts.php",
+		"List Suppliers" => "listSuppliers.php");
+
+?>
