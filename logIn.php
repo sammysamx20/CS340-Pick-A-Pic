26f530cfac8610a5c8f8b083a2ca3eb460e3658f
Sam Young <youngsam@oregonstate.edu>
2018-06-01 14:39:09 -0700

    added sign in function

--- logIn.php
+++ logIn.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+Username<!DOCTYPE html>
 <!-- Add Part Info to Table Part -->
 <html>
 	<head>
@@ -10,7 +10,46 @@
 
 
 
+	<?php
 
+		$msg = "Logging In";
+
+	// change the value of $dbuser and $dbpass to your Username and password
+		include 'connectvars.php';
+
+		$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		if (!$conn) {
+			die('Could not connect: ' . mysql_error());
+		}
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	// Escape user inputs for security
+			$Username = mysqli_real_escape_string($conn, $_POST['Username']);
+	  $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+
+
+	// See if pid is already in the table
+	$queryIn = "SELECT password FROM FinUser where Username='$Username' ";
+	$resultIn = mysqli_query($conn, $queryIn);
+	if ($row = mysqli_fetch_assoc($resultIn)){
+
+	  $saltSql = "SELECT * FROM FinUser WHERE Username='$Username' AND password = '$password'";
+	  $finalResult = mysqli_query($conn, $saltSql);
+	  if($finalrow = mysqli_fetch_assoc($finalResult)){
+	    echo "Success, you have logged in!";
+	  return true;
+	}
+	}
+	echo "Failed, please click the Log In tab and try again.";
+	return false;
+	}
+
+
+	// close connection
+	mysqli_close($conn);
+
+	?>
 	<?php include 'header.php';?>
 	<section>
     <h2> Logging In </h2>
@@ -19,8 +58,8 @@
 <fieldset>
 	<legend>Sign In!:</legend>
     <p>
-        <label for="username">Username:</label>
-        <input type="text"  class="required" name="username" id="username">
+        <label for="Username">Username:</label>
+        <input type="text"  class="required" name="Username" id="Username">
     </p>
   <p>
         <label for="password">Password:</label>
